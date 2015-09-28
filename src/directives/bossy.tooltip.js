be95783a0a51e435ecf1b212e817997529f1b8a1
albertoarmienta <albertoarmienta@gmail.com>
2015-09-27 21:23:30 -0700

    testing some alternate way to implement tooltip.

--- src/directives/bossy.tooltip.js
+++ src/directives/bossy.tooltip.js
@@ -2,21 +2,52 @@ function TooltipController($scope){
 
 }
 
-function Tooltip()
+function Tooltip($compile,$document)
 {
+  //var temp = '<div style="border: 1px solid black;"><div style="background-color: gray">yoooo</div><ng-transclude></ng-transclude></div>';
   return {
     restrict: 'A',
     scope: {
-      data: '='
+      data: '@'
     },
+    //transclude: true,
+    link: function(scope, element, attrs){
+      //$window.alert("yoo");
+      var tip = $compile('<div>data</div>')(scope);
+      //tipClassName = 'tooltip',
+///      tipActiveClassName = 'tooltip-show';
+
+
+      element.bind('mouseover', function (e) 
+      {
+        console.log(e.target.getBoundingClientRect());
+        var pos = e.target.getBoundingClientRect().height;
+        element.addClass('bossy-tooltip');
+        var changes = {
+          //color : "#ffffff",
+          left: pos.left,
+          content: 'yooooo',
+        };
+        element.css(changes);
+        //element.style.backgroundColor = "#ffffff";
+ //       element.addClass(tipActiveClassName);
+
+      });
+
+      element.bind('mouseout', function(){
+        element.removeClass('bossy-tooltip');
+      });
+    },
+
+   // template: temp,
     controller: TooltipController
   };
 }
 
-Tooltip.$inject = [];
+Tooltip.$inject = ['$compile','$document'];
 
-TooltipController.$inject = ['$scope'];
+TooltipController.$inject = ['$scope','$compile'];
 
 angular.module('bossy.tooltip', [])
-  .controller('bossyTooltipController', TooltipController)
-    .directive('bossTooltip', Tooltip);
+.controller('bossyTooltipController', TooltipController)
+.directive('bossyTooltip', Tooltip);
