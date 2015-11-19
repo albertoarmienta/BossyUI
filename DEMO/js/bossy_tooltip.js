0af81ca482f7c521bcc60d4a5bcfb683c4da6065
Adam <madannep@gmail.com>
2015-11-18 22:33:58 -0800

    Updating the demo files again. Finished all the examples just need to change what css and js I'm referenecing.

--- DEMO/js/bossy_tooltip.js
+++ DEMO/js/bossy_tooltip.js
@@ -1,4 +1,3 @@
-
 function TooltipController($scope){
 
   $scope.setAlignment = function(alignment){
@@ -7,10 +6,10 @@ function TooltipController($scope){
 
     if (alignment){
       if (alignment.toLowerCase() === 'left'){
-        alignmentClass = 'tooltip-left';
+        alignmentClass = 'bossy-tooltip-align-left';
       }
       else if (alignment.toLowerCase() === 'right'){
-        alignmentClass ='tooltip-right';
+        alignmentClass ='bossy-tooltip-align-right';
       }
     }
 
@@ -19,7 +18,6 @@ function TooltipController($scope){
 
   $scope.setActive = function(persist){
     // Force tooltip to persist without hovering
-
     var activeClass = '';
 
     if (persist){
@@ -36,13 +34,13 @@ function TooltipController($scope){
 
     if(position){
       if(position.toLowerCase() === 'left'){
-        positionClass = 'tooltip-pos-left';
+        positionClass = 'bossy-tooltip-pos-left';
       }
       else if(position.toLowerCase() === 'right'){
-        positionClass = 'tooltip-pos-right';
+        positionClass = 'bossy-tooltip-pos-right';
       }
       else if(position.toLowerCase() === 'bottom'){
-        positionClass = 'tooltip-pos-bottom';
+        positionClass = 'bossy-tooltip-pos-bottom';
       }
     }
 
@@ -52,11 +50,12 @@ function TooltipController($scope){
   $scope.setContentType = function(type){
     // Content type
     var contentType = '';
-    if ($scope.options.type){
-      if ($scope.options.type.toLowerCase() === 'html'){
+
+    if (type){
+      if (type.toLowerCase() === 'html'){
         contentType = 'content-html';
       }
-      else if ($scope.options.type.toLowerCase() === 'download'){
+      else if (type.toLowerCase() === 'download'){
         contentType = 'download';
       }
     }
@@ -64,27 +63,45 @@ function TooltipController($scope){
     return contentType;
   };
 
-  function initialize(){
+  $scope.setIconFloat = function(direction){
+    // Float icon to left or right
+    var iconDirection = '';
 
-    // Throw an error if text is not given
-    if (!$scope.data.text && $scope.options.transclude !== true){
-      console.error('You must include content for tool tip.');
+    if (direction){
+      if (direction.toLowerCase() === 'left'){
+        iconDirection = 'icon-left';
+      }
+      else if (direction.toLowerCase() === 'right'){
+        iconDirection = 'icon-right';
+      }
     }
 
-    // Fail safe in case options are not given
-    if (!$scope.options){
-      $scope.options = {
-        align: 'center',
-        color: 'black',
-        type: 'default',
-        transclude: false,
-        persist: false,
-        progress: '0',
-        icon: '',
-      };
-    }
+    return iconDirection;
+  };
 
+function initialize(){
+
+  // Throw an error if text is not given
+  if (!$scope.data.text && $scope.options.transclude !== true){
+    console.error('You must include content for tool tip.');
   }
+
+  // Fail safe in case options are not given
+  if (!$scope.options){
+    $scope.options = {
+      align: 'center',
+      position: 'top',
+      color: 'black',
+      type: 'default',
+      transclude: false,
+      persist: false,
+      progress: '0',
+      icon: '',
+      iconFloat:'left',
+    };
+  }
+
+}
 
   initialize();
 
@@ -124,21 +141,28 @@ function Tooltip()
       }
 
     },
-    template: '<span class="bossy-tooltip default-style">' +
-      '<span class="link">' +
-      '<span style="white-space: nowrap;">' +
-      '<ng-transclude></ng-transclude>' +
-      '</span>' +
-      '<div class="tooltip-active {{options.color.toLowerCase()}} {{setActive(options.persist)}} ' +
-      '{{setAlignment(options.align)}} {{setContentType(options.type)}} {{setPositioning(options.position)}}">' +
-      '<span ng-bind-html="data.text | bossyUnsafeHtml"></span>' +
-      '<i ng-show="options.icon" class="icon ionicon {{options.icon.toLowerCase()}}"></i>' +
-      '<div ng-show="options.type.toLowerCase() === \'download\'" class="progress-bar" style="width: {{options.progress}}%"></div>' +
-      '</div>' +
-      '</span>' +
-      '</span>',
+    template: '<span class="bossy-tooltip">' +
+                '<span class="link">' +
+                  '<ng-transclude></ng-transclude>' +
+                  '<div class="bossy-tooltip-active {{options.color.toLowerCase()}} {{setActive(options.persist)}} ' +
+                    '{{setAlignment(options.align)}} {{setContentType(options.type)}} {{setPositioning(options.position)}}">' +
+                    '<span ng-bind-html="data.text | bossyUnsafeHtml"></span>' +
+                    '<i ng-show="options.icon" class="icon ionicon {{options.icon.toLowerCase()}} {{setIconFloat(options.iconFloat)}}"></i>' +
+                    '<div ng-show="options.type.toLowerCase() === \'download\'" class="progress-bar" style="width: {{options.progress}}%"></div>' +
+                  '</div>' +
+                '</span>' +
+              '</span>',
   };
 }
+
+Tooltip.$inject = [];
+
+TooltipController.$inject = ['$scope'];
+
+angular.module('bossy.tooltip', ['bossy.filters'])
+.controller('bossyTooltipController', TooltipController)
+.directive('bossyTooltip', Tooltip);
+
 var app = angular.module('myApp', ["bossy.filters"]);
 app.controller('appCtrl', function($scope) {
   $scope.directiveData = {text: "This is default text in the tooltip"};
@@ -154,20 +178,77 @@ app.controller('appCtrl', function($scope) {
   };
   $scope.directiveData3 = {text: "This is default text in the tooltip"};
   $scope.directiveOptions3 = {
-    color:"green",
+    color:"green"
   };
   $scope.directiveData4 = {text: "This is default text in the tooltip"};
   $scope.directiveOptions4 = {
-    color:"orange",
+    color:"orange"
   };
   $scope.directiveData5 = {text: "This is default text in the tooltip"};
   $scope.directiveOptions5 = {
-    color:"red",
+    color:"red"
   };
   $scope.directiveData6 = {text: "This is default text in the tooltip"};
   $scope.directiveOptions6 = {
-    color:"blue",
+    color:"blue"
   };
+  $scope.directiveData7 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions7 = {
+    position:"left"
+  };
+  $scope.directiveData8 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions8 = {
+    position:"right"
+  };
+  $scope.directiveData9 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions9 = {
+    position:"bottom"
+  };
+  $scope.directiveData10 = {
+    text: "<img src=\"http://i.imgur.com/2yY7WNF.jpg\"><b>This is bold text within the tooltip</b><span>that is normal text in the tooltip</span>"
+  };
+  $scope.directiveOptions10 = {
+    type: "html"
+  };
+  $scope.directiveData11 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions11 = {
+    type: "download",
+    persist: true
+  };
+  $scope.directiveData12 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions12 = {
+    persist: true
+  };
+  $scope.directiveData13 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions13 = {
+    icon: "ion-scissors",
+    iconFloat: "left"
+  };
+  $scope.directiveData14 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions14 = {
+    transclude: true
+  };
+  $scope.directiveData15 = {text: "This is default text in the tooltip"};
+  $scope.directiveOptions15 = {
+    icon: "ion-scissors",
+    iconFloat: "right"
+  };
+  $scope.progressBar = function(){
+    for (i = 0; i < 100; i++){
+      this.directiveOptions11.progress = i;
+    }
+  };
+  $scope.progressReset = function(){
+    this.directiveOptions11.progress = 0;
+  }
+  $scope.togglePersist = function(){
+    if (this.directiveOptions12.persist) {
+      this.directiveOptions12.persist = false;
+    }
+    else {
+      this.directiveOptions12.persist = true;
+    }
+  }
 });
 app.directive('bossyTooltip', Tooltip);
 
